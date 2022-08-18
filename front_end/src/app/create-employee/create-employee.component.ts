638a7c994abc5ccef5a38a5c1800fa1e9d3cb8f3
VINAY Y <vinay.y3216@gmail.com>
2022-08-18 20:33:46 +0530

    first commit

---
+++ front_end/src/app/create-employee/create-employee.component.ts
@@ -0,0 +1,42 @@
+import { EmployeeService } from './../employee.service';
+import { Employee } from './../employee';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+
+  employee:Employee = new Employee();
+
+  constructor(private employeeService:EmployeeService,
+    private router:Router) { }
+
+  ngOnInit(): void {
+  }
+  onSubmit()
+  {
+    console.log(this.employee);
+    this.saveEmployee();
+  }
+
+  saveEmployee()
+  {
+    this.employeeService.createEmployee(this.employee).subscribe(data =>{
+      console.log(data);
+      this.goToEmployeeList();
+    },
+    error=>console.log(error));
+  }
+
+  goToEmployeeList()
+  {
+    this.router.navigate(['/employees']);
+
+  }
+
+ 
+}
---
+++ front_end/src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,42 @@
+import { Router } from '@angular/router';
+import { EmployeeService } from './../employee.service';
+import { Employee } from './../employee';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+
+  employees!: Employee[];
+
+  constructor(private employeeService: EmployeeService,
+    private router:Router) { }
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+  getEmployees()
+  {
+    this.employeeService.getEmployeesList().subscribe(data =>{
+      this.employees=data;
+    });
+  }
+  employeeDetails(id: number){
+    this.router.navigate(['employee-details', id]);
+  }
+  updateEmployee(id:number)
+  {
+    this.router.navigate(['update-employee', id]);
+
+  }
+  deleteEmployee(id: number){
+    this.employeeService.deleteEmployee(id).subscribe( data => {
+      console.log(data);
+      this.getEmployees();
+    })
+  }
+
+}
